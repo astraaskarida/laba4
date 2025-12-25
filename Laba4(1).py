13f6251651ce2a180c2d7ecfa5e1e1f23bfc2fa0
Семёнов Влад <139633734+astraaskarida@users.noreply.github.com>
2025-12-25 22:11:38 +0300

    Add files via upload

---
+++ Laba4(1).py
@@ -0,0 +1,30 @@
+# import math
+#
+# number = 25
+# result = math.sqrt(number)
+# print("Квадратный корень из", number, "равен", result)
+
+
+# 1)
+from math import sqrt
+
+def task_1 (number):
+    return sqrt(number)
+
+result = float(input("Введите число: "))
+
+print(task_1(result))
+
+# 2)
+from datetime import datetime
+
+time_right_now = datetime.now()
+
+print("Текущие дата и время: ", time_right_now)
+
+
+
+
+
+
+
